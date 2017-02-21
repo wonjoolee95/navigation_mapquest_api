9e97d13ec68513cc8774207a8cce7045f3179d17
Won Joo Lee <jeromelee95@gmail.com>
2017-02-21 15:33:03 -0800

    Updated user_interface.py

--- user_interface.py
+++ user_interface.py
@@ -1,29 +1,25 @@
-# _author = Won Joo Lee
+# Won Joo Lee
 
 import classes
 import mapquest_api
 
-
-
-def receive_locations(number_of_locations) -> list:
+def receive_locations() -> list:
     '''
-    given a number of locations,
     returns a list of locations with that many inputs
     '''
-    return [input('Please enter the address: ') for i in range(number_of_locations)]
+    locations = []
+    locations.append(input("FROM: "))
+    locations.append(input("TO: "))
+    return locations
 
-
-def receive_commands(number_of_commands, mapquest_json, elevation_json_list) -> list:
+def receive_commands(mapquest_json, elevation_json_list) -> list:
     '''
     returns a list of duck-typable class constructors,
     which does not need to specify which classes since
     all class values can be duck-typed by the fucntion print_data()
     '''
     final_commands = []
-    commands = [input(
-        'Please enter your #{} desired data type: \n'
-        'Options: STEPS, LATLONG, TOTALDISTANCE, TOTALTIME, ELEVATION \n'
-        'Type precisely: '.format(i+1)) for i in range(number_of_commands)]
+    commands = ["STEPS", "LATLONG", "TOTALDISTANCE", "TOTALTIME"]
     for command in commands:
         if command == 'STEPS':
             final_commands.append(classes.Steps(mapquest_json))
@@ -37,10 +33,6 @@ def receive_commands(number_of_commands, mapquest_json, elevation_json_list) -> 
             final_commands.append(classes.Elevation(elevation_json_list))
     return final_commands
                                   
-        
-    
-
-
 def receive_mapquest_json(locations: list) -> 'two jsons':
     '''
     given the list of places, returns two things:
@@ -57,9 +49,6 @@ def receive_mapquest_json(locations: list) -> 'two jsons':
         elevation_json_list.append(mapquest_api.receive_response(url))
         
     return mapquest_json, elevation_json_list
-        
-
-
 
 def run_user_interface() -> None:
     '''
@@ -67,36 +56,14 @@ def run_user_interface() -> None:
     calls appropriate fucntions to produce final data;
     has a try, except structure to catch a very general failure
     '''
+    print()
     print('Welcome to the GPS system!')
     print()
-    try:
-        while True:
-            initial_command = input('How many locations will the trip consist? ')
-            try:
-                initial_command = int(initial_command)
-            except:
-                print('Please enter a number: ')
-            else:
-                break
+    try:                                
+        locations = receive_locations()
         print()
-                                  
-        locations = receive_locations(initial_command)
-        print()
-        mapquest_json, elevation_json_list = receive_mapquest_json(locations)
-        while True:
-            second_command = input(
-                'How many types of data do you want? \n'
-                  'Options: STEPS, LATLONG, TOTALDISTANCE, TOTALTIME, ELEVATION \n'
-                  '(Enter a number): ')
-            try:
-                second_command = int(second_command)
-            except:
-                print('Please enter a number: ')
-            else:
-                break
-        print()
-                
-        commands = receive_commands(second_command, mapquest_json, elevation_json_list)
+        mapquest_json, elevation_json_list = receive_mapquest_json(locations)                
+        commands = receive_commands(mapquest_json, elevation_json_list)
         print()
         for i in commands:
             i.print_data()
